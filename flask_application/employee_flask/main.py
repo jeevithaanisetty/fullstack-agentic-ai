64e18546e3486b7335f9489caa56130b8859f27b
jeevithaanisetty <jeevitha.anisetty@thezennialpro.com>
2025-06-15 09:45:32 +0530

    added changes

--- flask_application/employee_flask/main.py
+++ flask_application/employee_flask/main.py
@@ -32,6 +32,10 @@ def employees_on_bench():
 def find_by_project_status():
     data=request.get_json()
     status=data.get("status")
+    if not status:
+         return jsonify({
+              "message":"status is required"
+         }),400
     result=[emp.to_dict() for emp in employees if emp.has_project_with_status(status)]
     logger.info(f"found employee with {status}")
     return jsonify(result)
@@ -42,6 +46,10 @@ def find_by_project_status():
 def find_by_department():
     data=request.get_json()
     department=data.get("department")
+    if not department:
+         return jsonify({
+              "message": "department is required"
+         }),400
     result=[emp.to_dict() for emp in employees if emp.department==department]
     logger.info(f"employees belongs to {department} dept are found")
     return jsonify(result)
@@ -52,6 +60,10 @@ def find_by_department():
 def find_by_designation():
     data=request.get_json()
     designation=data.get("designation")
+    if not designation:
+         return jsonify({
+              "message": "designation is required"
+         }),400
     result=[emp.to_dict() for emp in employees if emp.designation==designation]
     logger.info(f"employees belongs to {designation} role are found")
     return jsonify(result)
@@ -62,6 +74,10 @@ def find_by_designation():
 def find_by_location():
     data=request.get_json()
     location=data.get("location")
+    if not location:
+         return jsonify({
+              "message":"location is required"
+         }),400
     result=[emp.to_dict() for emp in employees if emp.location==location]
     logger.info(f"employees belongs to {location} are found")
     return jsonify(result)
@@ -73,9 +89,14 @@ def find_by_salary():
     data=request.get_json()
     min_salary=data.get("minimum_salary")
     max_salary=data.get("maximum_salary")
-    result=[emp.to_dict() for emp in employees if min_salary<=emp.salary>=max_salary]
-    logger.info(f"employees having salary in between {min_salary} ,{max_salary} are found")
-    return jsonify(result)
+    if min_salary is None or max_salary is None:
+         return jsonify({
+              "message" :" enter min and max salary"
+         }),400
+    else:
+        result=[emp.to_dict() for emp in employees if min_salary<=emp.salary<=max_salary]
+        logger.info(f"employees having salary in between {min_salary} ,{max_salary} are found")
+        return jsonify(result)
 
 # Searching employee having age in between x and y
 @handle_exceptions
@@ -84,7 +105,11 @@ def find_emp_by_age():
     data=request.get_json()
     min_age=data.get("minimum_age")
     max_age=data.get("maximum_age")
-    result=[emp.to_dict() for emp in employees if min_age<=emp.get_age()>=max_age]
+    if  min_age is None or max_age is None:
+         return jsonify({
+              "message":"enter min and max age limit"
+         }),400
+    result=[emp.to_dict() for emp in employees if min_age<=emp.get_age()<=max_age]
     logger.info(f"employees with age in between {min_age} and {max_age} are found")
     return jsonify(result)
 
@@ -94,6 +119,10 @@ def find_emp_by_age():
 def find_emp_by_above_age():
     data=request.get_json()
     age=data.get("age")
+    if not age:
+         return jsonify({
+              "message":"age is required"
+         }),400
     result=[emp.to_dict() for emp in employees if emp.get_age()>=age]
     logger.info(f"employees having age more than {age} are found")
     return jsonify(result)
@@ -104,6 +133,10 @@ def find_emp_by_above_age():
 def find_by_project():
     data=request.get_json()
     name=data.get("name")
+    if not name:
+         return jsonify({
+              "message":"project name is required"
+         })
     result=[emp.to_dict() for emp in employees for p in emp.projects if p.name==name ]
     logger.info(f"employees belongs to {name} project are found")
     return jsonify(result)
