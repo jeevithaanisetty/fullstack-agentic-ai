6b2b760fe6c87adcc84f8707d92e1722f7f0163b
jeevithaanisetty <jeevitha.anisetty@thezennialpro.com>
2025-06-17 09:56:36 +0530

    modified code with logging and exception handling decorator

--- gpt4/prompt_chaining/app/chain.py
+++ gpt4/prompt_chaining/app/chain.py
@@ -1,6 +1,24 @@
 from fastapi import APIRouter,HTTPException
 from pydantic import BaseModel
 from openai import OpenAI
+from functools import wraps
+import os
+import logging
+
+logging.basicConfig(
+    filename="prompt_chain.log",
+    level= logging.INFO,
+    format= "%(asctime)s [%(levelname)s]   %(message)s ")
+
+def handle_exceptions(func):
+    @wraps(func)
+    def wrapper (*args, **kwargs):
+        try:
+            logging.info(f"Executing: {func.__name__}")
+            return func(*args, **kwargs)
+        except Exception as e:
+            logging.exception(f"Error while Executing: {func.__name__} : {e}")
+    return wrapper
 
 router=APIRouter()             #we have to define router.here apirouter recieves request and send it to required
 
@@ -21,6 +39,7 @@ async def prompt_chain(input:TextInput):
         ],
         temperature=0.4
     ).choices[0].message.content.strip()    # to remove unneccessary content except message in response 
+    logging.info("summarized the input_text successfully")
 
     Grammer_check=client.chat.completions.create(
         model="gpt4",
@@ -29,7 +48,8 @@ async def prompt_chain(input:TextInput):
             {"role":"user","content":input.text}
         ],
         temperature=0.3
-    ).choices[0].message.content.strip()   
+    ).choices[0].message.content.strip()
+    logging.info("grammer fixed for the given input_text")   
     
     Translated_text=client.chat.completions.create(
         model="gpt4",
@@ -39,6 +59,7 @@ async def prompt_chain(input:TextInput):
         ],
         temperature=0.3
     ).choices[0].message.content.strip()
+    logging.info(f"input_text is translated to {input.language}successfully.if not check content carefullyand correct")
 
     return {
         "Summary":Summary,
