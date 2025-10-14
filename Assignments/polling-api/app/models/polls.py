72404a6f42016a77c74e2ba5ad1c3835542bb4d5
jeevithaanisetty <jeevitha.anisetty@thezennialpro.com>
2025-10-14 10:53:23 +0530

    cleared the bugs raised in tavily searching and AI integration in poll option suggestion is WIP

--- Assignments/polling-api/app/models/polls.py
+++ Assignments/polling-api/app/models/polls.py
@@ -4,7 +4,9 @@ from typing import List
 class PollCreate(BaseModel):
     question: str
     options: List[str]
- 
+    summary:str | None=None
+    related_info:list |None=None
+
 class Vote(BaseModel):
     poll_id:str
     option: int
--- Assignments/polling-api/app/services/services.py
+++ Assignments/polling-api/app/services/services.py
@@ -18,15 +18,18 @@ model=genai.GenerativeModel("gemini-2.5-flash")
 # tavily
 client=TavilyClient(api_key=TAVILY_API_KEY)
 
-async def tavily_search(query):
+async def tavily_searching(query):
     url="https://api.tavily.com/search"
-    response=tavily_search(query)
+    response=client.search(query=query,search_depth="basic")
+    result_urls=[item["url"] for item in response["results"]]
+    print(result_urls)
     logger.info("related content is found with tavily search")
-    return response
+    return result_urls
 
 async def summarize_article(article):
     prompt=f"Summarize this news article in 2 lines:\n\nTitle:{article["title"]}\n\nContent:{article.get("content","")}"
     response=model.generate_content(prompt)
+    print(response.text)
     logger.info("article is summarized using genai")
     return response.text.strip()
 
@@ -64,10 +67,10 @@ async def fetch_news_articles():
     return articles
 
 @handle_exceptions
-async def create_poll_service(data, source_url=None):
+async def create_poll_service(data,source_url=None):
     expires_at = datetime.utcnow() + timedelta(hours=24)
     options = [{"text": opt, "votes": 0} for opt in (data.options)]
-    
+
     new_poll = {
         "question": data.question,
         "options": options,
@@ -75,10 +78,10 @@ async def create_poll_service(data, source_url=None):
         "expires_at": expires_at,
         "is_active": True,
         "summary":data.summary,
-        "related_info":data.related_info,
+        "related_content":data.related_info,
         "source_url": source_url
     }
-    result =  db.polls.insert_one(new_poll)
+    result =await db.polls.insert_one(new_poll)
     return db.polls.find_one({"_id": result.inserted_id})    #return result
 
 @handle_exceptions
@@ -107,44 +110,38 @@ async def article_to_poll():
                     }}
                     Do not include any text outside JSON.
                     """
+        response=model.generate_content(prompt)
+        ai_output=response.text   #str
+        #print(type(ai_output))
         try:
-            response=model.generate_content(prompt)
-            ai_output=response.text   #str
-            #print(type(ai_output))
-            try:
-                output=json.loads(ai_output)  #dict
-                #print(type(output))
-            except json.JSONDecodeError:
-                match = re.search(r'\{.*\}', ai_output, re.DOTALL)
-                if match:
-                    try:
-                        output = json.loads(match.group())
-                    except json.JSONDecodeError:
-                        print("Still invalid JSON:", match.group())
+            output=json.loads(ai_output)  #dict
+            #print(type(output))
+        except json.JSONDecodeError:
+            match = re.search(r'\{.*\}', ai_output, re.DOTALL)
+            if match:
+                try:
+                    output = json.loads(match.group())
+                except json.JSONDecodeError:
+                    print("Still invalid JSON:", match.group())
 
-            question=output.get("question",f"Do you agree with this news? {title}")
-            options=output.get("options",["Agree", "Disagree","Neutral","None"])
-            
-            article_summary=await summarize_article(article)
-            tavily_response=await tavily_search(article["title"])
+        question=output.get("question",f"Do you agree with this news? {title}")
+        options=output.get("options",["Agree", "Disagree","Neutral","None"])
+        
+        article_summary=await summarize_article(article)
+        tavily_response=await tavily_searching(article["title"])
 
-            poll_info= PollCreate(
-                question=question,
-                options=options,
-                duration_minutes=1440,  # default 24 hours
-                summary=article_summary,
-                related_info=tavily_response 
-            )
-            url = article.get("url")
-            existing = db.polls.find_one({"$or": [{"question": question}, {"source_url": url}]})
-            if existing:
-                continue
-            created = await create_poll_service(poll_info, source_url=article.get("url"))
-            created_polls.append(created)
-        except Exception as e:
-            logger.error("AI poll generation is failed")
-            print("AI poll generation failed:{e}")
+        poll_info= PollCreate(
+            question=question,
+            options=options,
+            duration_minutes=1440,  # default 24 hours 
+            summary=article_summary,
+            related_info=tavily_response
+        )
+        url = article.get("url")
+        existing = db.polls.find_one({"$or": [{"question": question}, {"source_url": url}]})
+        if existing:
             continue
-
+        created = create_poll_service(poll_info, source_url=article.get("url"))
+        created_polls.append(created)
     return created_polls
 
